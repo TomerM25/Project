67c043e6e51ff48d1ade0a26a0d2a9f6c5869756
TomerM25 <tomer00001@gmail.com>
2023-05-25 16:38:01 +0300

    .

--- clean_environment.py
+++ clean_environment.py
@@ -1,8 +1,17 @@
 import requests
 
 
-# Send a GET request to stop rest_app server
-requests.get(f'http://127.0.0.1:5000/stop_server')
+try:
+    # Send a GET request to stop rest_app server
+    requests.get(f'http://127.0.0.1:5000/stop_server')
+    print(f'SUCCESS: rest_app is stopped')
+except:
+    print(f'Error in stopping rest_app server')
 
-# Send a GET request to stop web_app server
-requests.get(f'http://127.0.0.1:5001/stop_server')
+
+try:
+    # Send a GET request to stop web_app server
+    requests.get(f'http://127.0.0.1:5001/stop_server')
+    print(f'SUCCESS: web_app is stopped')
+except:
+    print(f'Error in stopping web_app server')
